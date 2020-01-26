036c62d76fdd48894272fea2d75683bca6703cfc
Pernille Dahl <pdahl@csumb.edu>
2020-01-25 20:21:28 -0800

    Initial Commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Pernille Dahl on 1/25/20.
+//  Copyright © 2020 Pernille Dahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
